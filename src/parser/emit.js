785f1ac1168d4929d573719950442818cb230249
Frank <365765383@qq.com>
2020-10-20 14:34:18 +0800

    refactor

--- src/parser/emit.js
+++ src/parser/emit.js
@@ -1,4 +1,5 @@
 const css = require("css");
+const { getSpecificity, match, compare } = require("./CSSUtil");
 let currentTextNode = null;
 let stack = [{ type: "document", children: [] }];
 
@@ -55,60 +56,6 @@ const computeCSS = (element) => {
       }
     }
   }
-};
-
-const compare = (sp1, sp2) => {
-  for (let i = 0; i < 4; i++) {
-    if (sp1[i] > sp2[i]) {
-      return true;
-    } else if (sp1[i] < sp2[i]) {
-      return false;
-    } else {
-      continue;
-    }
-  }
-
-  return true;
-};
-
-const getSpecificity = (selectors) => {
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
-  for (let selector of selectors) {
-    if (selector.charAt(0) === "#") {
-      i++;
-    } else if (selector.charAt(0) === ".") {
-      j++;
-    } else {
-      k++;
-    }
-  }
-
-  return [0, i, j, k];
-};
-
-const match = (element, selector) => {
-  if (!selector || !element.attributes) {
-    return false;
-  }
-  if (selector.charAt(0) === "#") {
-    const attr = element.attributes.filter((attr) => attr.name === "id")[0];
-    if (attr && attr.value === selector.replace("#", "")) {
-      return true;
-    }
-  } else if (selector.charAt(0) === ".") {
-    const attr = element.attributes.filter((attr) => attr.name === "class")[0];
-    if (attr && attr.value === selector.replace(".", "")) {
-      return true;
-    }
-  } else {
-    if (element.tagName === selector) {
-      return true;
-    }
-  }
-  return false;
 };
 
 const emit = (token) => {
